ae0da82f5e7d6f4eb5a703b22457c12230755235
ddbonilla <ddbonillav@gmail.com>
2023-01-28 23:32:26 -0800

    finish adding all hml pages

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.getElementsByClassId('back').style.display="none";
+
+
+
+
+// const searchInput = document.querySelector("[data-search]");
+
+// searchInput.addEventListener("input", e => {
+//     const value = e.target.value;
+//     console.log(value);
+// });
+
+// let movies = [];
